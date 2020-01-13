a207fd927e563f26f4a3229b591341138b1f98f2
FlorianClanet <florianclanet@gmail.com>
2020-01-13 23:12:11 +0100

    Implements polynomial regression and generic regression template

--- README.md
+++ README.md
@@ -26,8 +26,20 @@ First code file can be found [here](./courses/MachineLearningAZ_Python_HandsOn/p
 * Simple Linear Regression algorithm overview
 * Method of ordinary least squares
 
-An example on how to use the simple linear regression to predict salaries depending on the number of years of experience [here](./courses/MachineLearningAZ_Python_HandsOn/regression/simple_linear_regression/simple_linear_regression.py)
+An example on how to use the simple linear regression to predict salaries depending on the number of years of experience [here](./courses/MachineLearningAZ_Python_HandsOn/regressions/simple_linear_regression/simple_linear_regression.py)
 
 * Multiple linear regression / How to build a model
 
-The code example for multiple linear regression can be found [here](./courses/MachineLearningAZ_Python_HandsOn/regression/multiple_linear_regression/multiple_linear_regression.py)
+The code example for multiple linear regression can be found [here](./courses/MachineLearningAZ_Python_HandsOn/regressions/multiple_linear_regression/multiple_linear_regression.py)
+
+## Day 4
+
+* Understood Polynomial regression / comparison with linear regression model
+
+A code example for Polynomial regression can be found [here](./courses/MachineLearningAZ_Python_HandsOn/regressions/polynomial_regression/polynomial_regression.py)
+
+* Build a generic regression template
+
+The template is [here](./courses/MachineLearningAZ_Python_HandsOn/regressions/regression_template.py)
+
+* Registered to the [AWS Innovate online conference (AI/ML Edition) ](https://aws.amazon.com/fr/events/aws-innovate/machine-learning/) in February
---
+++ courses/MachineLearningAZ_Python_HandsOn/regressions/polynomial_regression/polynomial_regression.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 13 21:31:56 2020
+
+@author: flo
+"""
+
+# Polynomial Regression
+
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+
+# Splitting into Training and testing set
+"""from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state = 0)
+"""
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
+
+# Fitting Linear regression to the dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+# Fitting the Polynomial regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+# Visualising the LR results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Salary depending on position (Linear Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+
+# Visualising the PR results
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Salary depending on position (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+
+# Predicting a result with LR model
+pos = np.reshape(6.5, (1, -1))
+lin_reg.predict(pos)
+
+# Predicting a result with PR model
+lin_reg_2.predict(poly_reg.fit_transform(pos))
+
+
+
+
+
+
+
